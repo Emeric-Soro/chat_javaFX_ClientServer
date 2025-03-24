fec5482dd73faa73a072dd13d4746d4ef3f039ea
Emeric-Soro <sorojamel@gmail.com>
2025-03-24 13:13:15 +0000

    Deuxieme commit encore - j'ai oublier de finir ReunionDemocratique

--- src/Dependances/Personne.java
+++ src/Dependances/Personne.java
@@ -1,26 +1,58 @@
 package Dependances;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Personne {
     private String login;
     private String motDePasse;
     private boolean connecter;
+    private List<Reunion> reunionsOrganisees;
+    private List<Reunion> reunionsParticipees;
 
-    public Personne(){
+    public Personne() {
         this.login = "";
         this.motDePasse = "";
         this.connecter = false;
+        this.reunionsOrganisees = new ArrayList<>();
+        this.reunionsParticipees = new ArrayList<>();
     }
 
-    public String getMotDePasse() {
-        return motDePasse;
+    public Personne(String login, String motDePasse) {
+        this.login = login;
+        this.motDePasse = motDePasse;
+        this.connecter = false;
+        this.reunionsOrganisees = new ArrayList<>();
+        this.reunionsParticipees = new ArrayList<>();
+    }
+
+    public boolean authentifier(String motDePasse) {
+        return this.motDePasse.equals(motDePasse);
+    }
+
+    public void ajouterReunionOrganisee(Reunion reunion) {
+        reunionsOrganisees.add(reunion);
+    }
+
+    public void ajouterReunionParticipee(Reunion reunion) {
+        reunionsParticipees.add(reunion);
+    }
+
+    public void retirerReunionParticipee(Reunion reunion) {
+        reunionsParticipees.remove(reunion);
     }
 
     public String getLogin() {
         return login;
     }
 
-    public void setConnecter(boolean connecter) {
-        this.connecter = connecter;
+    public String getMotDePasse() {
+        return motDePasse;
+    }
+
+    public boolean isConnecte() {
+        return connecter;
     }
 
     public void setLogin(String login) {
@@ -30,4 +62,30 @@ public class Personne {
     public void setMotDePasse(String motDePasse) {
         this.motDePasse = motDePasse;
     }
+
+    public void setConnecte(boolean connecte) {
+        this.connecter = connecte;
+    }
+
+    public List<Reunion> getReunionsOrganisees() {
+        return reunionsOrganisees;
+    }
+
+    public List<Reunion> getReunionsParticipees() {
+        return reunionsParticipees;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Personne personne = (Personne) o;
+        return Objects.equals(login, personne.login);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(login);
+    }
+
 }
--- src/Dependances/ReunionDemocratique.java
+++ src/Dependances/ReunionDemocratique.java
@@ -1,10 +1,50 @@
 package Dependances;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
-public class ReunionDemocratique extends Reunion{
+public class ReunionDemocratique extends Reunion {
     private Queue<Personne> fileAttente;
     private Personne intervenantActuel;
 
-    public ReunionDemocratique(String nom, String sujet, String dateDebut, int duree, String ordreJourTexte, Personne orgaanisateur, )
+    public ReunionDemocratique(String nom, String sujet, String dateDebut, int duree, String ordreJourTexte, Personne orgaanisateur, Personne animateur) {
+        super(nom, sujet, dateDebut, duree, ordreJourTexte, orgaanisateur, animateur);
+        this.fileAttente = new LinkedList<>();
+        this.intervenantActuel = null;
+    }
+
+    @Override
+    public boolean demanderParole(Personne personne) {
+        if (ouverte && participants.containsKey(personne) && !fileAttente.contains(personne) && intervenantActuel != personne) {
+            fileAttente.add(personne);
+            participants.put(personne, true);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Personne choisirProchainIntervenant() {
+        if(!fileAttente.isEmpty()){
+            intervenantActuel = fileAttente.poll();
+            return intervenantActuel;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean peutIntervenir(Personne personne){
+        return personne.equals(intervenantActuel);
+    }
+
+    @Override
+    public boolean ajouterIntervention(Personne personne, String texte){
+        boolean result = super.ajouterIntervention(personne, texte);
+        if(result){
+            intervenantActuel = null;
+//            Après une intervention on prend le suivant automatiquemmmennnnt
+            choisirProchainIntervenant();
+        }
+        return result;
+    }
 }
